2baae00dec2aec7f21a56e5ab93bf15a48ae904b
Walkers <wakwaujaim@gmail.com>
2025-04-10 10:28:39 +0700

    initial commit

---
+++ src/main/java/id/putra/cropzilla/CropzillaApplication.java
@@ -0,0 +1,13 @@
+package id.putra.cropzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropzillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropzillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/putra/cropzilla/CropzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package id.putra.cropzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
