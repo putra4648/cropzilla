3a1abca164287805d09cc11ea7c80430bf3cc203
putra4648 <wakwaujaim@gmail.com>
2025-04-20 21:30:28 +0700

    feat: added pre-config for authorities

--- src/main/java/id/putra/cropzilla/config/WebSecurity.java
+++ src/main/java/id/putra/cropzilla/config/WebSecurity.java
@@ -1,13 +1,28 @@
 package id.putra.cropzilla.config;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
+
+import id.putra.cropzilla.converter.AuthoritiesConverter;
 
 @Configuration
 @EnableWebSecurity
@@ -37,5 +52,31 @@ public class WebSecurity {
                         .deleteCookies("JSESSIONID") // Deletes the session cookie
                 );
         return http.build();
+    }
+
+    @Bean
+    AuthoritiesConverter realmRolesAuthoritiesConverter() {
+        return claims -> {
+            var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
+            var roles = realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
+            return roles.map(List::stream)
+                    .orElse(Stream.empty())
+                    .map(SimpleGrantedAuthority::new)
+                    .map(GrantedAuthority.class::cast)
+                    .toList();
+        };
+    }
+
+    @Bean
+    GrantedAuthoritiesMapper authenticationConverter(
+            Converter<Map<String, Object>, Collection<GrantedAuthority>> authoritiesConverter) {
+        return (authorities) -> authorities.stream()
+                .filter(authority -> authority instanceof OidcUserAuthority)
+                .map(OidcUserAuthority.class::cast)
+                .map(OidcUserAuthority::getIdToken)
+                .map(OidcIdToken::getClaims)
+                .map(authoritiesConverter::convert)
+                .flatMap(roles -> roles.stream())
+                .collect(Collectors.toSet());
     }
 }
